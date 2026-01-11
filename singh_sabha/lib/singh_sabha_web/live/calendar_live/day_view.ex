3e80970b9e6472cdc86a9f982b71b634d3956136
Bhavanvir Rai <bhavanvir.r@gmail.com>
2026-01-11 14:43:46 -0800

    Move out CalenderTimeline into its own component

--- singh_sabha/lib/singh_sabha_web/live/calendar_live/day_view.ex
+++ singh_sabha/lib/singh_sabha_web/live/calendar_live/day_view.ex
@@ -2,7 +2,7 @@ defmodule SinghSabhaWeb.CalendarLive.DayView do
   use Phoenix.Component
 
   import SinghSabhaWeb.Helpers.CalendarHelpers
-  alias SinghSabhaWeb.CalendarLive.Components.DayViewMultiDayEventsRow
+  alias SinghSabhaWeb.CalendarLive.Components.{DayViewMultiDayEventsRow, CalendarTimeline}
 
   def view(assigns) do
     {single_day_events, multi_day_events} = partition_events(assigns.events)
@@ -106,53 +106,11 @@ defmodule SinghSabhaWeb.CalendarLive.DayView do
               <% end %>
             </div>
 
-            <.calendar_timeline current_time={@current_time} hours={@hours} />
+            <CalendarTimeline.view current_time={@current_time} hours={@hours} />
           </div>
         </div>
       </div>
     </div>
     """
-  end
-
-  defp calendar_timeline(assigns) do
-    now = assigns.current_time
-    current_hour = now.hour
-    current_minute = now.minute
-
-    hours_list = assigns.hours
-    first_hour = List.first(hours_list)
-    last_hour = List.last(hours_list)
-
-    show_timeline = current_hour >= first_hour and current_hour <= last_hour
-
-    if show_timeline do
-      minutes = current_hour * 60 + current_minute
-      visible_start_minutes = first_hour * 60
-      visible_end_minutes = (last_hour + 1) * 60
-      visible_range_minutes = visible_end_minutes - visible_start_minutes
-      position = (minutes - visible_start_minutes) / visible_range_minutes * 100
-
-      time_string = format_time(now)
-
-      assigns =
-        assigns
-        |> assign(:position, position)
-        |> assign(:time_string, time_string)
-
-      ~H"""
-      <div
-        class="pointer-events-none absolute inset-x-0 z-50 border-t border-black"
-        style={"top: #{@position}%"}
-      >
-        <div class="absolute left-0 top-0 size-3 -translate-x-1/2 -translate-y-1/2 rounded-full bg-black">
-        </div>
-        <div class="absolute -left-18 flex w-16 -translate-y-1/2 justify-end bg-base-100 pr-1 text-xs font-medium text-black">
-          {@time_string}
-        </div>
-      </div>
-      """
-    else
-      ~H""
-    end
   end
 end
