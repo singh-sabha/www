172715a3dba17d15d2ab5e8ba3d51882e28fa888
Bhavanvir Rai <bhavanvir.r@gmail.com>
2026-01-13 19:03:56 -0800

    Add minimal implementation of mini calendar in day view

--- singh_sabha/lib/singh_sabha_web/live/calendar_live/day_view.ex
+++ singh_sabha/lib/singh_sabha_web/live/calendar_live/day_view.ex
@@ -1,5 +1,6 @@
 defmodule SinghSabhaWeb.CalendarLive.DayView do
   use Phoenix.Component
+  use SinghSabhaWeb, :html
 
   import SinghSabhaWeb.Helpers.CalendarHelpers
   alias SinghSabhaWeb.CalendarLive.Components.{DayViewMultiDayEventsRow, CalendarTimeline}
@@ -43,72 +44,104 @@ defmodule SinghSabhaWeb.CalendarLive.DayView do
       |> assign(:hours, hours)
 
     ~H"""
-    <div class="flex flex-1 flex-col border-t border-base-300">
-      <div>
-        <DayViewMultiDayEventsRow.row
-          current_date={@current_date}
-          multi_day_events={@multi_day_events}
-        />
+    <div class="flex flex-row">
+      <div class="flex flex-1 flex-col border-t border-base-300">
+        <div>
+          <DayViewMultiDayEventsRow.row
+            current_date={@current_date}
+            multi_day_events={@multi_day_events}
+          />
 
-        <div class="relative z-20 flex border-b border-base-300">
-          <div class="w-18"></div>
-          <span class="flex-1 border-l border-base-300 py-2 text-center text-xs font-medium text-base-400">
-            {Calendar.strftime(@current_date, "%a")}
-            <span class="ml-1 font-semibold text-base-content">
-              {@current_date.day}
+          <div class="relative z-20 flex border-b border-base-300">
+            <div class="w-18"></div>
+            <span class="flex-1 border-l border-base-300 py-2 text-center text-xs font-medium text-base-400">
+              {Calendar.strftime(@current_date, "%a")}
+              <span class="ml-1 font-semibold text-base-content">
+                {@current_date.day}
+              </span>
             </span>
-          </span>
-        </div>
-      </div>
-
-      <div class="overflow-auto hide-scrollbar max-h-[800px]">
-        <div class="flex">
-          <div class="relative w-18 flex-shrink-0">
-            <%= for {hour, index} <- Enum.with_index(@hours) do %>
-              <div class="relative h-[96px]">
-                <%= if index != 0 do %>
-                  <div class="absolute -top-3 right-2 flex h-6 items-center">
-                    <span class="text-xs text-base-400">{format_hour(hour)}</span>
-                  </div>
-                <% end %>
-              </div>
-            <% end %>
           </div>
+        </div>
 
-          <div class="relative flex-1 border-l border-base-300">
-            <div class="relative">
+        <div class="overflow-auto hide-scrollbar max-h-[800px]">
+          <div class="flex">
+            <div class="relative w-18 flex-shrink-0">
               <%= for {hour, index} <- Enum.with_index(@hours) do %>
-                <% is_working = working_hour?(@current_date, hour, @working_hours) %>
-                <div class={["relative h-[96px]", !is_working && "bg-calendar-disabled-hour"]}>
+                <div class="relative h-[96px]">
                   <%= if index != 0 do %>
-                    <div class="pointer-events-none absolute inset-x-0 top-0 border-b border-base-300">
+                    <div class="absolute -top-3 right-2 flex h-6 items-center">
+                      <span class="text-xs text-base-400">{format_hour(hour)}</span>
                     </div>
                   <% end %>
-
-                  <div class="pointer-events-none absolute inset-x-0 top-1/2 border-b border-dashed border-base-300">
-                  </div>
-                </div>
-              <% end %>
-
-              <%= for {event, group_index, overlapping_indices} <- @events_with_overlap_info do %>
-                <% total_overlapping = length(overlapping_indices) %>
-                <% relative_position = Enum.find_index(overlapping_indices, &(&1 == group_index)) %>
-                <% style =
-                  get_event_style(event, relative_position, total_overlapping, @hours) %>
-                <div class="absolute p-1" style={style}>
-                  <div class="h-full rounded-md bg-blue-100 text-blue-800 px-2 py-1 text-xs overflow-hidden">
-                    <div class="font-medium truncate">{event.title}</div>
-                    <div class="text-blue-800">
-                      {format_time(event.start)} - {format_time(event.end)}
-                    </div>
-                  </div>
                 </div>
               <% end %>
             </div>
 
-            <CalendarTimeline.view current_time={@current_time} hours={@hours} />
+            <div class="relative flex-1 border-l border-base-300">
+              <div class="relative">
+                <%= for {hour, index} <- Enum.with_index(@hours) do %>
+                  <% is_working = working_hour?(@current_date, hour, @working_hours) %>
+                  <div class={["relative h-[96px]", !is_working && "bg-calendar-disabled-hour"]}>
+                    <%= if index != 0 do %>
+                      <div class="pointer-events-none absolute inset-x-0 top-0 border-b border-base-300">
+                      </div>
+                    <% end %>
+
+                    <div class="pointer-events-none absolute inset-x-0 top-1/2 border-b border-dashed border-base-300">
+                    </div>
+                  </div>
+                <% end %>
+
+                <%= for {event, group_index, overlapping_indices} <- @events_with_overlap_info do %>
+                  <% total_overlapping = length(overlapping_indices) %>
+                  <% relative_position = Enum.find_index(overlapping_indices, &(&1 == group_index)) %>
+                  <% style =
+                    get_event_style(event, relative_position, total_overlapping, @hours) %>
+                  <div class="absolute p-1" style={style}>
+                    <div class="h-full rounded-md bg-blue-100 text-blue-800 px-2 py-1 text-xs overflow-hidden">
+                      <div class="font-medium truncate">{event.title}</div>
+                      <div class="text-blue-800">
+                        {format_time(event.start)} - {format_time(event.end)}
+                      </div>
+                    </div>
+                  </div>
+                <% end %>
+              </div>
+
+              <CalendarTimeline.view current_time={@current_time} hours={@hours} />
+            </div>
           </div>
         </div>
+      </div>
+
+      <div class="hidden lg:block border-l border-t border-base-300">
+        <calendar-date class="cally border-b border-base-300" first-day-of-week="0" locale="en-CA">
+          <svg
+            aria-label="Previous"
+            slot="previous"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+          </svg>
+          <svg
+            aria-label="Next"
+            slot="next"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+          </svg>
+          <calendar-month></calendar-month>
+        </calendar-date>
       </div>
     </div>
     """
