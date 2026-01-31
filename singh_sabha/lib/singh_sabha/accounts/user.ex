df580c0da8c20bbdf360aad5d961dc1d45b6081c
Bhavanvir Rai <bhavanvir.r@gmail.com>
2026-01-31 15:29:40 -0800

    Add `is_admin` field to user accounts

--- singh_sabha/lib/singh_sabha/accounts/user.ex
+++ singh_sabha/lib/singh_sabha/accounts/user.ex
@@ -8,6 +8,7 @@ defmodule SinghSabha.Accounts.User do
     field :hashed_password, :string, redact: true
     field :confirmed_at, :utc_datetime
     field :authenticated_at, :utc_datetime, virtual: true
+    field :is_admin, :boolean, default: false
 
     timestamps(type: :utc_datetime)
   end
