167d283b6ba5a8af9bc12a81bfb101908059c6bc
Bhavanvir Rai <bhavanvir.r@gmail.com>
2026-01-31 15:39:55 -0800

    Should be red instead of green

--- singh_sabha/lib/singh_sabha_web/live/calendar_live/day_view.ex
+++ singh_sabha/lib/singh_sabha_web/live/calendar_live/day_view.ex
@@ -170,8 +170,8 @@ defmodule SinghSabhaWeb.CalendarLive.DayView do
           <%= if length(current_events) > 0 do %>
             <div class="flex items-start gap-2 px-4 pt-4">
               <span class="relative mt-[5px] flex h-2.5 w-2.5">
-                <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-success opacity-75" />
-                <span class="relative inline-flex h-2.5 w-2.5 rounded-full bg-success" />
+                <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-error opacity-75" />
+                <span class="relative inline-flex h-2.5 w-2.5 rounded-full bg-error" />
               </span>
               <p class="text-sm font-semibold">Happening now</p>
             </div>
