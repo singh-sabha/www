59f0a794aa98aaf5bffc8b847f636d219aeb7a25
Bhavanvir Rai <bhavanvir.r@gmail.com>
2026-02-19 17:11:40 -0800

    Fix issue with wrong channel ID and incorrect class application

--- singh_sabha/lib/singh_sabha_web/live/home_live/live_stream_section.ex
+++ singh_sabha/lib/singh_sabha_web/live/home_live/live_stream_section.ex
@@ -2,7 +2,7 @@ defmodule SinghSabhaWeb.HomeLive.LiveStreamSection do
   use Phoenix.Component
   use SinghSabhaWeb, :html
 
-  @channel_id "UCNYMuETXWtm6Nh1R4wGESIQ"
+  @channel_id "UCtMxxM3Lr4qf8_EzrQORhQg"
 
   attr :live_stream, :map, default: nil
 
@@ -22,7 +22,9 @@ defmodule SinghSabhaWeb.HomeLive.LiveStreamSection do
       </p>
 
       <%= if @live_stream do %>
-        <.live_stream_card live_stream={@live_stream} />
+        <div class="pt-8">
+          <.live_stream_card live_stream={@live_stream} />
+        </div>
       <% else %>
         <div class="flex flex-col items-center justify-center gap-2 rounded-box pt-8">
           <.icon name="hero-video-camera-slash" class="size-10 opacity-70" />
@@ -48,7 +50,7 @@ defmodule SinghSabhaWeb.HomeLive.LiveStreamSection do
 
   defp live_stream_card(assigns) do
     ~H"""
-    <div class="mx-auto overflow-hidden rounded-box border border-base-300 pt-8">
+    <div class="mx-auto overflow-hidden rounded-box border border-base-300">
       <div class="p-4 flex items-center justify-center">
         <p class="font-semibold">{@live_stream.title}</p>
       </div>
