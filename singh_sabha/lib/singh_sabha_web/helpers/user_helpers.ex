f92c1e0492076786cf1802230bb714e350196a79
Bhavanvir Rai <bhavanvir.r@gmail.com>
2026-02-22 13:51:48 -0800

    Expand gradient pallete

--- singh_sabha/lib/singh_sabha_web/helpers/user_helpers.ex
+++ singh_sabha/lib/singh_sabha_web/helpers/user_helpers.ex
@@ -1,6 +1,6 @@
 defmodule SinghSabhaWeb.Helpers.UserHelpers do
-  def generate_gradient_colours(email) do
-    hash = :erlang.phash2(email)
+  def generate_gradient_colours(user_id) do
+    hash = :erlang.phash2(user_id)
 
     colors = [
       {"#667eea", "#764ba2"},
@@ -10,11 +10,38 @@ defmodule SinghSabhaWeb.Helpers.UserHelpers do
       {"#fa709a", "#fee140"},
       {"#30cfd0", "#330867"},
       {"#a8edea", "#fed6e3"},
-      {"#ff9a56", "#ff6a88"}
+      {"#ff9a56", "#ff6a88"},
+      {"#f7971e", "#ffd200"},
+      {"#21d4fd", "#b721ff"},
+      {"#08aeea", "#2af598"},
+      {"#8ec5fc", "#e0c3fc"},
+      {"#d4fc79", "#96e6a1"},
+      {"#f77062", "#fe5196"},
+      {"#c471ed", "#12c2e9"},
+      {"#f64f59", "#c0392b"},
+      {"#56ab2f", "#a8e063"},
+      {"#eecda3", "#ef629f"},
+      {"#2193b0", "#6dd5ed"},
+      {"#cc2b5e", "#753a88"},
+      {"#42275a", "#734b6d"},
+      {"#bdc3c7", "#2c3e50"},
+      {"#de6161", "#2657eb"},
+      {"#1a1a2e", "#16213e"},
+      {"#f3904f", "#3b4371"},
+      {"#0f0c29", "#302b63"},
+      {"#e96443", "#904e95"},
+      {"#00b09b", "#96c93d"},
+      {"#fd746c", "#ff9068"},
+      {"#3f2b96", "#a8c0ff"}
     ]
 
     {color1, color2} = Enum.at(colors, rem(hash, length(colors)))
     "#{color1}, #{color2}"
+  end
+
+  def generate_user_id do
+    symbols = ~c"0123456789abcdefghiklmnopqrstuvwxyz"
+    for _ <- 1..10, into: "", do: <<Enum.random(symbols)>>
   end
 
   def is_admin?(%SinghSabha.Accounts.Scope{user: user}), do: user.is_admin
