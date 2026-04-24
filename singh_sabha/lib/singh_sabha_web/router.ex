955e8e9a62e4530a9c291b5633c1ac4c85a367a2
Bhavanvir Rai <bhavanvir.r@gmail.com>
2026-04-23 23:34:31 -0700

    Add a notification page

--- singh_sabha/lib/singh_sabha_web/router.ex
+++ singh_sabha/lib/singh_sabha_web/router.ex
@@ -91,6 +91,7 @@ defmodule SinghSabhaWeb.Router do
       ] do
       live "/users/settings", UserLive.Settings, :edit
       live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+      live "/users/notifications", UserLive.Notifications, :index
     end
 
     post "/users/update-password", UserSessionController, :update_password
