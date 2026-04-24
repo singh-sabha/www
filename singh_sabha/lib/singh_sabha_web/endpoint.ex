3819c6f8f823681caa462d83c204cbdffa649db1
Bhavanvir Rai <bhavanvir.r@gmail.com>
2026-04-23 23:32:19 -0700

    Allow longpolling the same properties

--- singh_sabha/lib/singh_sabha_web/endpoint.ex
+++ singh_sabha/lib/singh_sabha_web/endpoint.ex
@@ -13,7 +13,7 @@ defmodule SinghSabhaWeb.Endpoint do
 
   socket "/live", Phoenix.LiveView.Socket,
     websocket: [connect_info: [:peer_data, :x_headers, session: @session_options]],
-    longpoll: [connect_info: [session: @session_options]]
+    longpoll: [connect_info: [:peer_data, :x_headers, session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
